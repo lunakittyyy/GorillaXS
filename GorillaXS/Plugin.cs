21645289fce8384d77a4f56743f729c0a5dd9a4c
luna <17483712+lunakittyyy@users.noreply.github.com>
2024-06-11 17:23:56 -0400

    my dumbass forgor that it uses utilla :skull:

--- GorillaXS/Plugin.cs
+++ GorillaXS/Plugin.cs
@@ -7,6 +7,7 @@ using System.Text;
 
 namespace GorillaXS
 {
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
     [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
     public class XSPlugin : BaseUnityPlugin
     {
