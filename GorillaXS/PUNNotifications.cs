fbd4744a0355d9921e31d0bff6883594ecbd998e
luna <translillie@gmail.com>
2024-06-11 16:25:57 -0400

    First commit

---
+++ GorillaXS/PUNNotifications.cs
@@ -0,0 +1,22 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+
+namespace GorillaXS
+{
+    internal class PUNNotifications : MonoBehaviourPunCallbacks
+    {
+        public override void OnPlayerEnteredRoom(Player newPlayer)
+        {
+            base.OnPlayerEnteredRoom(newPlayer);
+
+            Plugin.Notify("Player Joined", $"{newPlayer.NickName} joined");
+        }
+
+        public override void OnPlayerLeftRoom(Player otherPlayer)
+        {
+            base.OnPlayerLeftRoom(otherPlayer);
+
+            Plugin.Notify("Player Left", $"{otherPlayer.NickName} left");
+        }
+    }
+}
---
+++ GorillaXS/Types/XSOApiObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GorillaXS.Types
+{
+    internal class XSOApiObject
+    {
+        public string sender = null;
+        public string target = null;
+        public string command = null;
+        public string jsonData = null;
+        public string rawData = null;
+    }
+}
